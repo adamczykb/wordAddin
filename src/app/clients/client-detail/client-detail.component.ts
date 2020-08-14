f2bad460017f192f32ae54b522a015e603b50b95
unknown <pawel.dopierala4@gmail.com>
2020-08-14 17:41:46 +0200

    dziala

--- src/app/clients/client-detail/client-detail.component.ts
+++ src/app/clients/client-detail/client-detail.component.ts
@@ -1,10 +1,33 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from 'rxjs';
+import { Client } from '../client';
+import { ClientService } from '../client.service';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
 @Component({
-    selector: 'client-detail',
+    selector: 'app-client-detail',
     templateUrl: './client-detail.component.html',
     styleUrls: ['./client-detail.component.css']
 })
 export class ClientDetailComponent implements OnInit {
-    constructor(){}
-    ngOnInit(){}
+    client$: Observable<Client>;
+
+    constructor(
+        private service: ClientService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {}
+
+    ngOnInit() {
+        this.client$ = this.route.paramMap.pipe(
+            switchMap((params: ParamMap) =>
+                this.service.getClient(params.get('id')))
+        );
+    }
+
+    gotoClients(client: Client) {
+        const clientId = client ? client.id : null;
+        this.router.navigate(['/clients', { id: clientId, foo: 'foo' }]);
+    }
 }
